f813b406d193d0964fe813fad0cd5d632c7e202d
Prabhi <prabhims@gmail.com>
2026-07-21 10:39:37 +0530

    Added changes

--- Basics15_Closures.js
+++ Basics15_Closures.js
@@ -1,4 +1,6 @@
 // closure provides access to an element from its outer scope even after the inner scope is closed
+// A closure is a function that remembers variables from its outer scope even
+// after the outer function has finished executing.
 function greet(){
     let name = "Prabhi";
     function displayname(){   // inner scope
